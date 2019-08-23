efb1b93cbc791dfa6c661c1ff1aed37ad8dce024
Talha Balaj <talhabalaj@hotmail.com>
2019-08-23 14:43:51 +0500

    typescript

---
+++ src/components/Home.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import * as styles from './Home.scss'
+
+class Home extends React.Component {
+  render() {
+    return <p className={styles.white}>Hello</p>
+  }
+}
+
+export default Home
--- webpack.config.js
+++ webpack.config.js
@@ -5,85 +5,79 @@ const HtmlWebPlugin = require('html-webpack-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const TerserJSPlugin = require('terser-webpack-plugin')
 
-module.exports = {
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env', '@babel/preset-react'],
-            },
-          },
-          'eslint-loader',
-        ],
-      },
-      {
-        test: /\.(css|scss|sass)$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              hmr: true,
-              reloadAll: true,
-            },
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-            },
-          },
-          'sass-loader',
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new BitBarWebpackProgressPlugin(),
-    new MiniCssExtractPlugin({
-      ignoreOrder: true,
-      filename: '[name].css',
-    }),
-    new HtmlWebPlugin({
-      template: './public/index.html',
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeRedundantAttributes: true,
-        useShortDoctype: true,
-        removeEmptyAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        keepClosingSlash: true,
-        minifyJS: true,
-        minifyCSS: true,
-        minifyURLs: true,
-      },
-    }),
-  ],
-  optimization: {
-    minimizer: [
-      new TerserJSPlugin({}),
-      new OptimizeCSSAssetsPlugin({}),
-    ],
-  },
-  devServer: {
-    clientLogLevel: 'silent',
-    bonjour: true,
-    hot: true,
-    inline: true,
-    stats: {
-      // Config for minimal console.log mess.
-      colors: true,
-      version: false,
-      hash: false,
-      timings: false,
-      chunks: false,
-      chunkModules: false,
+module.exports = env => {
+  return {
+    mode: env,
+    entry: ['./src/index.tsx'],
+    resolve: {
+      extensions: ['.ts', '.tsx', '.scss', '.js', 'jsx'],
     },
-  },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          use: ['ts-loader'],
+        },
+        {
+          test: /\.(css|scss|sass)$/,
+          use: [
+            {
+              loader:
+                env === 'development'
+                  ? 'style-loader'
+                  : MiniCssExtractPlugin.loader,
+              options:
+                env === 'development'
+                  ? undefined
+                  : {
+                      hmr: true,
+                    },
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                localsConvention: 'camelCase',
+              },
+            },
+            'sass-loader',
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new BitBarWebpackProgressPlugin(),
+      new MiniCssExtractPlugin({
+        ignoreOrder: true,
+        filename: '[name].css',
+      }),
+      new HtmlWebPlugin({
+        template: './public/index.html',
+        minify: {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true,
+          removeEmptyAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          keepClosingSlash: true,
+          minifyJS: true,
+          minifyCSS: true,
+          minifyURLs: true,
+        },
+      }),
+    ],
+    optimization: {
+      minimizer: [
+        new TerserJSPlugin({}),
+        new OptimizeCSSAssetsPlugin({}),
+      ],
+    },
+    devServer: {
+      clientLogLevel: 'silent',
+      bonjour: true,
+      inline: true,
+    },
+  }
 }
